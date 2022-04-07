de12bd5d06d6d2f0fbbe08d4882ccb51f3262d84
Ashley <apark8496@gmail.com>
2022-04-07 15:51:29 -0400

    basic outline for header, footer added

--- breakingcode/src/App.js
+++ breakingcode/src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, from 'react';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+
 
 function App() {
   return (

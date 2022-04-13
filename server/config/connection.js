16c2ae07b8ba879c12878d877d7b101c44bd4593
Trevor L <trevorlong992@gmail.com>
2022-04-13 19:14:13 -0400

    Semantic correction

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb+srv://TrevorLong:TheBigOof992@cluster0.ywscg.mongodb.net/cards?retryWrites=true&w=majority',
+  process.env.MONGODB_URI || 'mongodb+srv://TrevorLong:TheBigOof992@cluster0.ywscg.mongodb.net/Cards?retryWrites=true&w=majority',
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,

12ec9885b6173e26e5b79244cee2dad053d289b4
Ashley <apark8496@gmail.com>
2022-04-12 20:57:19 -0400

    working on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,79 +1,80 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import {
-//   ApolloClient,
-//   InMemoryCache,
-//   ApolloProvider,
-//   createHttpLink,
-// } from '@apollo/client';
-// import { setContext } from '@apollo/client/link/context';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import Header from './components/header';
-// import About from '../src/pages/about';
+import About from '../src/pages/about';
 import Game from '../src/pages/game';
 // import Scoreboard from '../src/pages/scoreboard';
 import Footer from './components/footer';
-// import Login from './components/login';
-// import Signup from './components/signup';
+import Login from '../src/pages/login';
+import Signup from '../src/pages/signup';
 
-
-function App() {
-  return (
-    <>
-      <Header />
-      <switch>
-        <About></About>
-        {/* <Game></Game> */}
-      </switch>
-
-      <Footer />
-
-    </>
-  );
-}
-
-export default App;
-
-
-// const httpLink = createHttpLink({
-//   uri: '/graphql',
-// });
-
-// const authLink = setContext((_, { headers }) => {
-//   const token = localStorage.getItem('id_token');
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : '',
-//     },
-//   };
-// });
-
-// const client = new ApolloClient({
-//   link: authLink.concat(httpLink),
-//   cache: new InMemoryCache(),
-// });
 
 // function App() {
 //   return (
-//     <ApolloProvider client={client}>
-//       <Router>
-//         <div className="flex-column justify-flex-start min-100-vh">
-//           <Header />
-//           <div className="container">
-//             <Switch>
-//               <Route exact path="/" component={About} />
-//               <Route exact path="/login" component={Login} />
-//               <Route exact path="/signup" component={Signup} />
-//               <Route exact path="/game" component={Game} />
-//               <Route exact path="/scoreboard" component={Scoreboard} />
-//             </Switch>
-//           </div>
-//           <Footer />
-//         </div>
-//       </Router>
-//     </ApolloProvider>
+//     <>
+//       <Header />
+//       <switch>
+//         {/* <About></About> */}
+//         <Game></Game>
+//       </switch>
+
+//       <Footer />
+
+//     </>
 //   );
 // }
 
 // export default App;
+
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route exact path="/About" component={About} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/game" component={Game} />
+              {/* <Route exact path="/scoreboard" component={Scoreboard} /> */}
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
+
 import Auth from '../utils/auth';
 
 const Login = (props) => {
@@ -38,46 +39,42 @@ const Login = (props) => {
     });
   };
 
-    return (
-        <>
-            <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-                <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-                    Something went wrong with your login credentials!
-                </Alert>
-                <Form.Field>
-                    <Form.Label htmlFor='email'>Email</Form.Label>
-                    <Form.Control
-                        type='text'
-                        placeholder='Your email'
-                        name='email'
-                        onChange={handleInputChange}
-                        value={userFormData.email}
-                        required
-                    />
-                    <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
-                </Form.Field>
+  return (
+    <main className="flex-row justify-center mb-4">
+      <div className="col-12 col-md-6">
+        <div className="card">
+          <h4 className="card-header">Login</h4>
+          <div className="card-body">
+            <form onSubmit={handleFormSubmit}>
+              <input
+                className="form-input"
+                placeholder="Your email"
+                name="email"
+                type="email"
+                id="email"
+                value={formState.email}
+                onChange={handleChange}
+              />
+              <input
+                className="form-input"
+                placeholder="******"
+                name="password"
+                type="password"
+                id="password"
+                value={formState.password}
+                onChange={handleChange}
+              />
+              <button className="btn d-block w-100" type="submit">
+                Submit
+              </button>
+            </form>
 
-                <Form.Field>
-                    <Form.Label htmlFor='password'>Password</Form.Label>
-                    <Form.Control
-                        type='password'
-                        placeholder='Your password'
-                        name='password'
-                        onChange={handleInputChange}
-                        value={userFormData.password}
-                        required
-                    />
-                    <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
-                </Form.Field>
-                <Button
-                    disabled={!(userFormData.email && userFormData.password)}
-                    type='submit'
-                    variant='success'>
-                    Submit
-                </Button>
-            </Form>
-        </>
-    );
+            {error && <div>Login failed</div>}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Login;

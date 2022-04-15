ce15c5e60fbc139b13311eb5ca8654522ce7cee3
Ashley <apark8496@gmail.com>
2022-04-14 23:02:16 -0400

    test branch

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = "mongodb+srv://TrevorLong:TheBigOof992@cluster0.ywscg.mongodb.net/Cards?retryWrites=true&w=majority";
-
-mongoose.connect(MONGODB_URI || 'mongodb://localhost/breakingcode_data', {
+mongoose.connect(MONGODB_URI || 'mongodb://localhost/breakingcode', {
   useNewUrlParser: true, 
   useUnifiedTopology: true,
 })
